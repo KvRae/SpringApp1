8f0b79e68bff4058cba3330c52e8f3c7ce76ada5
KvRae <karamelmannai@gmail.com>
2022-10-03 18:55:21 +0100

    adding universtie table test

--- src/main/java/com/example/springapp1/SpringApp1Application.java
+++ src/main/java/com/example/springapp1/SpringApp1Application.java
@@ -1,5 +1,7 @@
 package com.example.springapp1;
 
+import com.example.springapp1.entity.Universite;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -14,12 +16,20 @@ public class SpringApp1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringApp1Application.class, args);
-    }
-    final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-            .configure() // configures settings from hibernate.cfg.xml
-            .build();
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
 
-    SessionFactory sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
-    Session session = sessionFactory.openSession();
-    Transaction transaction = session.beginTransaction();
+        SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Universite universite = new Universite();
+        universite.setNomUniv("Université de Paris");
+
+        session.save(universite);
+        transaction.commit();
+        session.close();
+        sessionFactory.close();
+    }
 }
